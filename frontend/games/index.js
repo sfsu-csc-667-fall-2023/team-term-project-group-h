e60cb7f858cb7b2f5887ed5b34ddad6a0c79e7ca
Justin Yu <justin741yu@gmail.com>
2023-12-09 11:22:07 -0800

    Error in fetch call in ready.js

--- frontend/games/index.js
+++ frontend/games/index.js
@@ -1,13 +1,10 @@
 import { configure as gameSocketConfig } from "./game_socket";
 import { configure as userSocketConfig } from "./user_socket";
+import { ready } from "./ready"
 
-const gameSocketId = document.querySelector("#game-socket-id").value;
-const userSocketId = document.querySelector("#user-socket-id").value;
-const roomId = document.querySelector("#roomId").value;
 
-gameSocketConfig(gameSocketId)
-  .then((_) => userSocketConfig(userSocketId))
-  .then((_) => {
-    console.log("Fetching");
-    fetch(`/game/${roomId}/ready`, { method: "post" });
-});
+
+const gameSocket = await gameSocketConfig()
+const userSocket = await userSocketConfig()
+
+ready(gameSocket, userSocket);
---
+++ frontend/games/ready.js
@@ -0,0 +1,36 @@
+const roomId = document.querySelector("#roomId").value;
+
+const connectionState = {
+    game: false,
+    user: false,
+    updated: Date.now(),
+  }
+  
+  const checkForReady = () => {
+    const { game, user } = connectionState;
+  
+    if (game && user) {
+      fetch(`/games/${roomId}/ready`, { method: "post" });
+    }
+  }
+  
+  const ready = (gameSocket, userSocket) => {
+  
+    console.log("READY CALLED", connectionState)
+  
+    gameSocket.on("connect", () => { 
+      connectionState.game = true;
+      connectionState.updated = Date.now();
+  
+      checkForReady();
+    })
+  
+    userSocket.on("connect", () => {
+      connectionState.user = true;
+      connectionState.updated = Date.now();
+  
+      checkForReady();
+    })
+  }
+  
+  export { ready }
