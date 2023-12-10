6fba39a0433b7f3904ac5fefcb29c9beb1c6c1e1
RINO-GAELICO <ducciorocca@gmail.com>
2023-12-10 11:42:02 -0800

    New Look for game and logic for game socket

--- frontend/games/game_socket.js
+++ frontend/games/game_socket.js
@@ -20,18 +20,32 @@ const playerTwoHand = document.querySelector(".player-two-hand");
 const playerThreeHand = document.querySelector(".player-three-hand");
 const playerFourHand = document.querySelector(".player-four-hand");
 
-const updateHand = (handContainer, cardList) => {
+const updateHand = (handContainer, cardList, game_id) => {
+  const suitsMap = {
+    0: "spades",
+    1: "clubs",
+    2: "hearts",
+    3: "diamonds",
+  };
+
+  
   handContainer.innerHTML = "";
 
+
   cardList.forEach(({ suits, value }) => {
+
     const container = cardTemplate.content.cloneNode(true);
     const div = container.querySelector(".card");
 
     div.classList.add(`suit-${suits}`);
     div.classList.add(`value-${value}`);
-    div.innerText = `${value}`;
+    
+    div.innerText = `${value} of ${suitsMap[suits]}`;
     // div.addEventListener("click", () => {
-    //   fetch(`/game/${roomId}/ready`, { method: "post", body: {
+    //   // change to opaque if not already present otherwise remove
+    //   div.classList.contains("opaque") ? div.classList.remove("opaque") : div.classList.add("opaque");
+      
+    //   fetch(`/game/${game_id}/play`, { method: "post", body: {
     //       "suit": suits,
     //       "value": value
     //     }
@@ -42,19 +56,35 @@ const updateHand = (handContainer, cardList) => {
   });
 };
 
+const updatePoints = (players) => {
+  players.forEach((player) => {
+    const playerPoints = document.getElementById(`player-${player.seat+1}-points`);
+    if (playerPoints) {
+      playerPoints.innerText = `USER: ${player.username}, POINTS: ${player.hand_points} `;
+      console.log(`Player ${player.seat} has ${player.game_points} game points, ${player.hand_points} hand points`);
+    } else {
+      console.error(`Element not found for seat ${player.seat}`);
+    }
+  });
+}
+
 const stateUpdated = ({ game_id, current_player, players }) => {
-  if(players.length === 2) {
+
+  if(players.length === 4) {
+
+    updatePoints(players);
     const seatZeroCards = players.find((player) => player.seat === 0).hand;
     const seatOneCards = players.find((player) => player.seat === 1).hand;
 
-    // const seatTwoCards = players.find((player) => player.seat === 2).hand;           commented out for easier testing with 2 players
-    // const seatThreeCards = players.find((player) => player.seat === 3).hand;
-    console.log({ seatZeroCards, seatOneCards });
-    updateHand(playerOneHand, seatZeroCards);
-    updateHand(playerTwoHand, seatOneCards);
-    // updateHand(playerThreeHand, seatThreeCards);
-    // updateHand(playerFourHand, seatFourCards);
+    const seatTwoCards = players.find((player) => player.seat === 2).hand;          // commented out for easier testing with 2 players
+    const seatThreeCards = players.find((player) => player.seat === 3).hand;
+    // console.log({ seatZeroCards, seatOneCards });
+    updateHand(playerOneHand, seatZeroCards,game_id);
+    updateHand(playerTwoHand, seatOneCards,game_id);
+    updateHand(playerThreeHand, seatTwoCards, game_id);
+    updateHand(playerFourHand, seatThreeCards, game_id);
   }
+
 };
 
 export { configure };
