bd81855a64fc75fcf901aa97fbe5a7524acbd399
Dat Vo <dvo2@mail.sfsu.edu>
2023-12-15 20:40:45 -0800

    cleaned up the rest of routes

--- backend/routes/authentication.js
+++ backend/routes/authentication.js
@@ -4,23 +4,22 @@ const { Users } = require("../db");
 const bcrypt = require("bcrypt");
 const SALT_ROUND = 10;
 
-//Login Page
 router.get("/", (request, response) => {
   response.render("login",{ links: { lobby: { href: "/lobby", text: "Lobby" } }});
 });
 
-//Login Post
 router.post("/", async (request, response) => {
   const { username, password } = request.body;
+
   try{
     const user = await Users.find_by_username(username);
     const isValidUser = await bcrypt.compare(password, user.password);
     if(isValidUser){
-      // TODO Store in session
       request.session.user = {
         id: user.id,
         username,
       };
+
       response.redirect("/lobby");
       return;
     } else {
@@ -32,31 +31,26 @@ router.post("/", async (request, response) => {
   }
 });
 
-//Signup route
 router.post("/sign_up", async (request, response) => {
   const { email, username, password } = request.body;
   
   const user_exists = await Users.username_exists(username);
   const email_exists = await Users.email_exists(email);
-  // First Check if they exist and redirect to login.
+
   if (user_exists || email_exists) {
     response.redirect("/login");
     return;
   }
-  // Encrypt the clear text password
+
   const salt = await bcrypt.genSalt(SALT_ROUND);
   const hash = await bcrypt.hash(password, salt);
-
-  //Store in the DB
   const user = await Users.create(email, username, hash);
 
-  //Store in session
   request.session.user = {
     id: user.id,
     username,
   };
 
-  //Redirect lobby
   response.redirect("/lobby");
 });
 
